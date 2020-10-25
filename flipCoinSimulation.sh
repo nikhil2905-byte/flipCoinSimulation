bfabe859505707f38fe734a8c683c167ce37e9f8
unknown <nikhilnakirikanti27@gmail.com>
2020-10-25 10:56:45 +0530

    add: Multiply Times Fliping A Coin

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -2,11 +2,21 @@
 echo "Welcome To Flip Coin Simulation"
 head=1;
 tail=0;
-flipcoincheck=$((RANDOM%2));
-if [ $flipcoincheck -eq $head ];
-then
+read -p "enter flipping times:" n;
+head_cnt=0
+tail_cnt=0
+for (( i=0; i<=n; i++ ))
+do
+	flipcoincheck=$((RANDOM%2));
+	if [ $flipcoincheck -eq $head ];
+	then
 	echo "Head"
-elif [ $flipcoincheck -eq $tail ]
-then
+	head_cnt=$(($head_cnt+1))
+	elif [ $flipcoincheck -eq $tail ]
+	then
 	echo "Tail"
+	tail_cnt=$(($tail_cnt+1))
 fi
+done
+echo "Number Of Times Heads Won :" $head_cnt
+echo "Number Of Times Tails Won :" $tail_cnt
