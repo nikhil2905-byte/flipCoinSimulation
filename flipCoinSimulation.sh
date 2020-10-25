9134789d3a3782dfe4dcd5d5a1dd48bed34f24b7
unknown <nikhilnakirikanti27@gmail.com>
2020-10-25 10:43:05 +0530

    add: Display Head Or Tails

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,2 +1,12 @@
 #!/bin/bash -x
 echo "Welcome To Flip Coin Simulation"
+head=1;
+tail=0;
+flipcoincheck=$((RANDOM%2));
+if [ $flipcoincheck -eq $head ];
+then
+	echo "Head"
+elif [ $flipcoincheck -eq $tail ]
+then
+	echo "Tail"
+fi
