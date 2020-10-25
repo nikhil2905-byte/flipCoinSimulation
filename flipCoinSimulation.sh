488d31525268f168abb669ba5e361c21bce5929d
unknown <nikhilnakirikanti27@gmail.com>
2020-10-25 11:13:45 +0530

    add: Flip 21 Times Show Win or Tie

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -2,21 +2,32 @@
 echo "Welcome To Flip Coin Simulation"
 head=1;
 tail=0;
-read -p "enter flipping times:" n;
 head_cnt=0
 tail_cnt=0
-for (( i=0; i<=n; i++ ))
+for (( i=0; i<=21; i++ ))
 do
 	flipcoincheck=$((RANDOM%2));
 	if [ $flipcoincheck -eq $head ];
 	then
-	echo "Head"
 	head_cnt=$(($head_cnt+1))
 	elif [ $flipcoincheck -eq $tail ]
 	then
-	echo "Tail"
 	tail_cnt=$(($tail_cnt+1))
 fi
 done
 echo "Number Of Times Heads Won :" $head_cnt
 echo "Number Of Times Tails Won :" $tail_cnt
+if [ $head_cnt -eq $tail_cnt ]
+then 
+	echo "Tie"
+elif [ $head_cnt -lt $tail_cnt ]
+then
+	echo "Tail wins"
+	won_by=$(($tail_cnt-$head_cnt))
+	echo "Tails won by:$won_by"
+elif [ $head_cnt -gt $tail_cnt ]
+then
+        echo "Head wins"
+	won_by=$(($head_cnt-$tail_cnt))
+        echo "Heads won by:$won_by"
+fi
