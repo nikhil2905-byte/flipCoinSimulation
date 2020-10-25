b49beb6e2b92999188c711a96524b7ed82accba2
unknown <nikhilnakirikanti27@gmail.com>
2020-10-25 11:27:15 +0530

    add : If Its Tie Difference Minumum Of 2 Points

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -4,7 +4,7 @@ head=1;
 tail=0;
 head_cnt=0
 tail_cnt=0
-for (( i=0; i<=21; i++ ))
+for (( i=1; i<=22; i++ ))
 do
 	flipcoincheck=$((RANDOM%2));
 	if [ $flipcoincheck -eq $head ];
@@ -17,17 +17,41 @@ fi
 done
 echo "Number Of Times Heads Won :" $head_cnt
 echo "Number Of Times Tails Won :" $tail_cnt
+a=$(($head_cnt-$tail_cnt))
+b=$(($tail_cnt-$head_cnt))
 if [ $head_cnt -eq $tail_cnt ]
-then 
-	echo "Tie"
+then
+        echo "Tie"
+        while [ $a -lt 2 -a $b -lt 2 ]
+	do
+		flipcoincheck=$((RANDOM%2));
+
+        	if [ $flipcoincheck -eq $head ];
+        	then
+                	head_cnt=$(( $head_cnt + 1 ))
+        	elif [ $flipcoincheck -eq $tail ]
+        	then
+                	tail_cnt=$(( $tail_cnt + 1 ))
+        	fi
+                a=$(($head_cnt-$tail_cnt))
+                b=$(($tail_cnt-$head_cnt))
+                if [ $head_cnt -lt $tail_cnt ]
+                then
+                        echo "Tail wins"
+                        echo "tail won by:$b"
+                elif [ $head_cnt -gt $tail_cnt ]
+                then
+                        echo "Head wins"
+                        echo "head won by:$a"
+                fi
+	done
 elif [ $head_cnt -lt $tail_cnt ]
 then
-	echo "Tail wins"
-	won_by=$(($tail_cnt-$head_cnt))
-	echo "Tails won by:$won_by"
+        echo "Tails Win"
+        echo "Tails Won by:$b"
 elif [ $head_cnt -gt $tail_cnt ]
 then
-        echo "Head wins"
-	won_by=$(($head_cnt-$tail_cnt))
-        echo "Heads won by:$won_by"
+        echo "Heads Wins"
+        echo "Heads Won by:$a"
 fi
+
